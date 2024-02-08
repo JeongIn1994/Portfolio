9400e20280fcded979ba33acc61bdfb24eba8c08
JeongIn1994 <be7814@gmail.com>
2024-02-08 23:19:22 +0900

    이력 등록 수정 / 서머리 이미지 출력 수정

--- src/main/java/com/practice/jeongin/portfolio/web/dto/PostsResponseDto.java
+++ src/main/java/com/practice/jeongin/portfolio/web/dto/PostsResponseDto.java
@@ -3,16 +3,28 @@ package com.practice.jeongin.portfolio.web.dto;
 import com.practice.jeongin.portfolio.domain.posts.Posts;
 import lombok.Getter;
 
+import java.util.Date;
+
 @Getter
 public class PostsResponseDto {
 
     private final Long id;
     private final String title;
     private final String content;
+    private final String language;
+    private final Date start_date;
+    private final Date end_date;
+    private final String summary;
+    private final long progress;
 
     public PostsResponseDto(Posts entity){
         this.id = entity.getId();
         this.title = entity.getTitle();
         this.content = entity.getContent();
+        this.language = entity.getLanguage();
+        this.summary = entity.getSummary();
+        this.start_date = entity.getStart_date();
+        this.end_date = entity.getEnd_date();
+        this.progress = entity.getProgress();
     }
 }
